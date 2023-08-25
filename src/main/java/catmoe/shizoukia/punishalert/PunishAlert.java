8c04f5e3f6d4f940e3ddc8f1f0b1501b2f8be80c
Akkariin <fallencrystal@outlook.com>
2023-08-25 17:25:56 +0800

    Update.

--- src/main/java/catmoe/shizoukia/punishalert/PunishAlert.java
+++ src/main/java/catmoe/shizoukia/punishalert/PunishAlert.java
@@ -51,8 +51,20 @@ public final class PunishAlert extends JavaPlugin implements CommandExecutor {
         super.onLoad();
     }
 
+    // 翻译自:
+    // https://github.com/CatMoe/MoeFilter/blob/stray/bungee/src/main/java/catmoe/fallencrystal/moefilter/util/message/v2/MessageUtil.kt#L89-L94
+    public static StringBuilder argsBuilder(int startIndex, String[] args) {
+        StringBuilder message = new StringBuilder();
+        if (args != null) {
+            for (int i = startIndex; i < args.length - 1; i++) { message.append(args[i]).append(" "); }
+        }
+        message.append(args != null ? args[args.length - 1] : message);
+        return message;
+    }
+
     // 此处用来获取CONSOLE的CommandSender静态对象 以在方法中调用
     public static final CommandSender CONSOLE = Bukkit.getConsoleSender();
+    public static final String unknown = "Unknown command. Type \"/help\" for help.";
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -76,21 +88,17 @@ public final class PunishAlert extends JavaPlugin implements CommandExecutor {
 
         但 实际上也不要指望switch能对于这种if else能有多复杂的判断或者拥有多个rule.
          */
-        switch (command.getName().toLowerCase()) {
-            case "punishalert":
-                if (args.length != 1) {
-                    sender.sendMessage("§cUsage: /punishalert <player>");
-                    return true;
-                }
-                // 检查玩家是否拥有特定权限
-                // *: 很好的实践 但实际上在plugin.yml中就可以指定插件权限 但也请考虑if (!sender.hasPermission("permission")) { return true }
-                if (sender.hasPermission("punishalert.sendmessage")) {
-                    Bukkit.getServer().broadcastMessage(
-                            colorize("\n&f&l» §c§l一名玩家因违反游戏规则被移出游戏！\n&f&l» §b§l使用/report来举报违反游戏规则的玩家！\n&f&l   ")
-                    );
 
-                    // 对于不需要重新赋值的对象 建议使用final关键字来声明对象不可被二次修改.
-                    final Player targetPlayer;
+        // 检查玩家是否拥有特定权限
+        // *: 还算可以的实践 但实际上在plugin.yml中就可以指定插件权限 但也请考虑if (!sender.hasPermission("permission")) { return true } (已修改)
+        // 在布尔值前面加感叹号可以反转布尔值 (颠倒黑白? 类似于true变成false false变成true之类的 *魔法*
+        if (!sender.hasPermission("punishalert")) {
+            sender.sendMessage(colorize(unknown));
+            return true;
+        }
+
+        // 对于不需要重新赋值的对象 建议使用final关键字来声明对象不可被二次修改.
+        final Player targetPlayer;
                     /*
                     关于 try-catch语句:
                     try-catch是用来捕获指定Throwable对象的 (例如Exception, Error, etc.)
@@ -103,65 +111,70 @@ public final class PunishAlert extends JavaPlugin implements CommandExecutor {
 
                     为了养成良好的编程语法习惯, 当不需要Exception本身的信息时, 应当命名为 "ignore" 或 "safeIgnore" 等字样
                      */
-                    try {
-                        targetPlayer = Bukkit.getPlayer(args[0]);
-                        // 是的, 我们可以通过 throw + 方法插入Throwable对象来抛出某个异常
-                        // 虽然无论如何我们抛出的NPE都会被下面的catch块处理
-                        if (targetPlayer == null) { throw new NullPointerException(); }
-                    } catch (IndexOutOfBoundsException | NullPointerException ignore) {
-                        sender.sendMessage(colorize("&cPlayer not found or not online."));
-                        return true;
-                    }
-                    targetPlayer.getWorld().strikeLightningEffect(targetPlayer.getLocation());
-                    // replace对于需要插入复杂的内容的情况下可以显得格外灵活.
-                    final String c = "litebans:ban [player] [reason] 7d -s"
-                            .replace("[player]", targetPlayer.getName())
-                            .replace("[reason]", colorize("&c使用第三方程序进行作弊 &f[A]"));
-                    Bukkit.getServer().dispatchCommand(CONSOLE, c);
-                } else {
-                    sender.sendMessage("Unknown command type /help for help");
+        try {
+            targetPlayer = Bukkit.getPlayer(args[0]);
+            // 是的, 我们可以通过 throw + 方法插入Throwable对象来抛出某个异常
+            // 虽然无论如何我们抛出的NPE都会被下面的catch块处理
+            if (targetPlayer == null) { throw new NullPointerException(); }
+        } catch (IndexOutOfBoundsException | NullPointerException ignore) {
+            sender.sendMessage(colorize("&cPlayer not found or not online."));
+            return true;
+        }
+        switch (command.getName().toLowerCase()) {
+            case "punishalert":
+                if (args.length != 1) {
+                    sender.sendMessage("§cUsage: /punishalert <player>");
+                    return true;
+                }
+                // 检查玩家是否拥有特定权限
+                if (!sender.hasPermission("punishalert.sendmessage")) {
+                    sender.sendMessage(colorize(unknown));
+                    return true;
                 }
 
+                Bukkit.getServer().broadcastMessage(
+                        colorize("\n&f&l» §c§l一名玩家因违反游戏规则被移出游戏！\n&f&l» §b§l使用/report来举报违反游戏规则的玩家！\n&f&l   ")
+                );
+                targetPlayer.getWorld().strikeLightningEffect(targetPlayer.getLocation());
+                // replace对于需要插入复杂的内容的情况下可以显得格外灵活.
+                final String banCommand = "litebans:ban [player] [reason] 7d -s"
+                        .replace("[player]", targetPlayer.getName())
+                        .replace("[reason]", colorize("&c使用第三方程序进行作弊 &f[A]"));
+                Bukkit.getServer().dispatchCommand(CONSOLE, banCommand);
+
             case "wipealert":
-                if (args.length != 2) {
+                // 如果想接收之后的reason args的长度不应该是必须等于2 应该是必须大于或等于2 (也就是不能小于1
+                if (args.length < 2) {
                     sender.sendMessage(colorize("&cUsage: /wipealert <player> <reason>"));
                     return true;
                 }
                 // 检查玩家是否拥有特定权限
-                if (sender.hasPermission("punishalert.sendmessage")) {
-
-                    final Player targetPlayer;
-                    try {
-                        targetPlayer = Bukkit.getPlayer(args[0]);
-                        if (targetPlayer == null) { throw new NullPointerException(); }
-                    } catch (IndexOutOfBoundsException | NullPointerException ignore) {
-                        sender.sendMessage(colorize("&cPlayer not found or not online."));
-                        return true;
-                    }
-
-                    // 稍微有些糟糕的实践注意: 如果reason需要捕获后面的更多内容 则可能需要使用StringBuilder append之后的内容
-                    // 只会捕获命令中第三个的arg 之后的就直接忽略掉了吧似乎
-                    final String reason;
-                    try {
-                        reason = args[1];
-                        if (reason == null || reason.isEmpty()) { throw new NullPointerException(); }
-                    } catch (IndexOutOfBoundsException | NullPointerException e) {
-                        sender.sendMessage(colorize("Please select a reason"));
-                        return true;
-                    }
-
-                    final String c = "wipe [player] [reason]"
-                            .replace("[player]", targetPlayer.getName())
-                            .replace("[reason]", reason);
-                    final String wipeWarn = "\n&f&l» &c&l您的账户部分游戏数据已被我们重置\n"
-                            + "&f&l» &6&l原因: &f&l[reason]\n".replace("[reason]", reason)
-                            + "&f&l» §b§l请遵守我们的游戏规则 不要使用违规增益\n&f&l» "
-                            + "详见: &ehttps://www.miaomoe.net/rules \n&f&l   ";
-                    targetPlayer.sendMessage(colorize(wipeWarn));
-                    Bukkit.getServer().dispatchCommand(CONSOLE, c);
-                } else {
-                    sender.sendMessage("Unknown command type /help for help");
+                if (!sender.hasPermission("punishalert.sendmessage")) {
+                    sender.sendMessage(colorize(unknown));
+                    return true;
                 }
+                // 稍微有些糟糕的实践注意: 如果reason需要捕获后面的更多内容 则可能需要使用StringBuilder append之后的内容 (已修改)
+                // 只会捕获命令中第三个的arg 之后的就直接忽略掉了吧似乎
+                final String reason;
+                try {
+                    // reason = args[1];
+                    // 来自L56的静态方法argsBuilder, 通过String.valueOf(obj) 将类型转换成String.
+                    reason = String.valueOf(argsBuilder(1, args));
+                    if (reason == null || reason.isEmpty()) { throw new NullPointerException(); }
+                } catch (IndexOutOfBoundsException | NullPointerException e) {
+                    sender.sendMessage(colorize("Please select a reason"));
+                    return true;
+                }
+
+                final String wipeCommand = "wipe [player] [reason]"
+                        .replace("[player]", targetPlayer.getName())
+                        .replace("[reason]", reason);
+                final String wipeWarn = "\n&f&l» &c&l您的账户部分游戏数据已被我们重置\n"
+                        + "&f&l» &6&l原因: &f&l[reason]\n".replace("[reason]", reason)
+                        + "&f&l» §b§l请遵守我们的游戏规则 不要使用违规增益\n&f&l» "
+                        + "详见: &ehttps://www.miaomoe.net/rules \n&f&l   ";
+                targetPlayer.sendMessage(colorize(wipeWarn));
+                Bukkit.getServer().dispatchCommand(CONSOLE, wipeCommand);
         }
         return true;
     }
